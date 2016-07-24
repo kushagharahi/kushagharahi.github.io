22dd2117a4b0b1254c658bb27a25926a0c21baa9
Kusha Gharahi <kusha95@gmail.com>
2016-07-24 18:19:03 -0500

    added some more styling

--- scripts/ts/controllers/mainController.ts
+++ scripts/ts/controllers/mainController.ts
@@ -4,6 +4,7 @@ module kushame {
     interface MainControllerScope {
         helloWorld: string;
         resumeUrl: string;
+        pogoMapUrl: string;
     }
 
     class MainController {
@@ -12,7 +13,8 @@ module kushame {
           $scope.helloWorld = "NG1 + TypeScript";
           $scope.resumeUrl =
             $sce.trustAsResourceUrl("https://docs.google.com/document/d/1vnGrGLXtvXVZ9E_Ih02o7cxjp1oxX_wi3Vg2n-m7aXs/pub?embedded=true");
-        }
+        $scope.pogoMapUrl = $sce.trustAsResourceUrl("http://159.203.176.43:8000");
+    }
     }
     app.controller("MainController", MainController);
 }
