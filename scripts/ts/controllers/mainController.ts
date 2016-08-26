ff81c76d579238e0e19ac87285b1f5a903e084e2
Kusha Gharahi <kusha95@gmail.com>
2016-08-25 23:10:09 -0500

    pogo map removal and css tweaks

--- scripts/ts/controllers/mainController.ts
+++ scripts/ts/controllers/mainController.ts
@@ -13,7 +13,6 @@ module kushame {
           $scope.helloWorld = "NG1 + TypeScript";
           $scope.resumeUrl =
             $sce.trustAsResourceUrl("https://docs.google.com/document/d/1vnGrGLXtvXVZ9E_Ih02o7cxjp1oxX_wi3Vg2n-m7aXs/pub?embedded=true");
-        $scope.pogoMapUrl = $sce.trustAsResourceUrl("http://159.203.176.43:8000");
     }
     }
     app.controller("MainController", MainController);
