45f09d03048855aca3fddcc9dd3dd413ba290d3c
Kusha Gharahi <kusha95@gmail.com>
2016-09-03 20:24:33 -0500

    added css compression and js minification

--- gulpFile.js
+++ gulpFile.js
@@ -4,11 +4,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
+var minify = require('gulp-minify');
 
 
 gulp.task('sassCompile', function () {
-  gulp.src('./res/style/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  gulp.src('./res/style/sass/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(gulp.dest('./res/style'));
 });
  
@@ -19,4 +20,17 @@ gulp.task('tsCompile', function() {
     return tsResult.js.pipe(gulp.dest('release'));
 });
 
-gulp.task('compile', ['tsCompile', 'sassCompile']);
+gulp.task('jsCompress', ['tsCompile'], function() {
+  gulp.src('./scripts/*.js')
+    .pipe(minify({
+        ext:{
+            src:'.js',
+            min:'-min.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('./scripts/'))
+});
+
+gulp.task('compile', ['tsCompile', 'sassCompile', 'jsCompress']);
