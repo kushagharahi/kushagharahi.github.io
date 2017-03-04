2da10b582e065f991049c2a99cfb944ce3a10777
Kusha Gharahi <kusha95@gmail.com>
2017-03-03 20:32:54 -0600

    starting on vanillajs version of site

--- scripts/app.js
+++ scripts/app.js
@@ -114,7 +114,7 @@ var kushame;
     "use strict";
     var MainController = (function () {
         function MainController($scope, $sce) {
-            var resumeUrl = "https://docs.google.com/document/d/1vnGrGLXtvXVZ9E_Ih02o7cxjp1oxX_wi3Vg2n-m7aXs/pub?embedded=true";
+            var resumeUrl = "https://docs.google.com/document/preview?authuser=0&amp;hgd=1&amp;id=1vnGrGLXtvXVZ9E_Ih02o7cxjp1oxX_wi3Vg2n-m7aXs";
             $scope.resumeUrl =
                 $sce.trustAsResourceUrl(resumeUrl);
         }
--- scripts/ts/controllers/mainController.ts
+++ scripts/ts/controllers/mainController.ts
@@ -8,7 +8,7 @@ module kushame {
     class MainController {
         public static $inject = ["$scope", "$sce"];
         constructor($scope: MainControllerScope, $sce: ng.ISCEService) {
-            let resumeUrl = "https://docs.google.com/document/d/1vnGrGLXtvXVZ9E_Ih02o7cxjp1oxX_wi3Vg2n-m7aXs/pub?embedded=true";
+            let resumeUrl = "https://docs.google.com/document/preview?authuser=0&amp;hgd=1&amp;id=1vnGrGLXtvXVZ9E_Ih02o7cxjp1oxX_wi3Vg2n-m7aXs/";
             $scope.resumeUrl =
                 $sce.trustAsResourceUrl(resumeUrl);
         }
