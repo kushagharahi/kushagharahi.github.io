05fe5b18e7b150f200a703a9478de928a42ef8bc
Kusha Gharahi <kusha95@gmail.com>
2017-04-02 23:25:22 -0500

    added MVP of blog

---
+++ src/scripts/components/BlogComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <div v-for="post in posts">
+        <h2>{{post.title}}</h2>
+        <router-link :to="'/blog/post?name=' + post.name">{{post.title}}</router-link>
+      </div>
+    </div>
+</template>
+
+<script>
+import postsJson from 'models/blog/posts.json'
+export default {
+  data: () => {
+    return {
+      posts: postsJson
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+
+</style>
