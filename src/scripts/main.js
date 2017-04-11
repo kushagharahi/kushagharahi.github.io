8dc7c2e7aea8721889fb0f5ad3a85e52e6f5f8f6
Kusha <kusha@Kushas-MacBook-Air.local>
2017-04-11 01:23:43 -0500

    title was too long

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,7 +20,7 @@ Vue.component('nav-component', NavComponent)
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: view('HomeComponent'), meta: { title: 'Kusha Gharahi - Software Engineer' }},
+    { path: '/', component: view('HomeComponent'), meta: { title: 'Kusha Gharahi' }},
     { path: '/resume', component: view('ResumeComponent'), meta: { title: 'Resume' }},
     { path: '/projects', component: view('ProjectsComponent'), meta: { title: 'Projects' }},
     { path: '/contact', component: view('ContactComponent'), meta: { title: 'Contact' }},
