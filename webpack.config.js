33463a5448dd4c06b1bd90f68a4589593880d5b2
Kusha Gharahi <kusha95@gmail.com>
2017-04-27 22:11:25 -0500

    starting on open graph tags

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 // Hack for Ubuntu on Windows: interface enumeration fails with EINVAL, so return empty.
-try {
-  require('os').networkInterfaces()
-} catch (e) {
-  require('os').networkInterfaces = () => ({})
-}
+// try {
+//   require('os').networkInterfaces()
+// } catch (e) {
+//   require('os').networkInterfaces = () => ({})
+// }
 
 var webpack = require('webpack')
 var path = require('path')
