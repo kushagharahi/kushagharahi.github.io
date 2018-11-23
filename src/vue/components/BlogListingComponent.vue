83b11ce91479fe9faf74747002b173f17962de0c
Kusha Gharahi <3326002+kushagharahi@users.noreply.github.com>
2018-11-22 23:14:17 -0600

    fixing blog post bug

--- src/vue/components/BlogListingComponent.vue
+++ src/vue/components/BlogListingComponent.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <div id="center" v-for="post in splicedPosts" v-bind:key="post.name">
+        <div id="center" v-for="post in posts" v-bind:key="post.name">
             <div class="post shadow hover">
                 <h2><router-link class="postTitle" :to="'/blog/' + post.name">{{post.title}}</router-link></h2>
                 <span class="postDate">{{post.date_posted}} </span>
                 <p>{{post.subtitle}}</p>
-                <p><router-link :to="'/blog/' + post.name+ '/'">Full post >></router-link></p>
+                <p><router-link :to="'/blog/' + post.name">Full post >></router-link></p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import postsJson from 'models/blog/posts.json'
+import posts from 'models/blog/posts.json'
 
 export default {
 props: {
@@ -20,15 +20,8 @@ props: {
 },
   data: () => {
     return {
-        posts: postsJson
+        posts
       }
-    },
-    computed: {
-        splicedPosts: function() {
-            let vm = this;
-            return vm.posts
-            //return vm.posts.splice(0, vm.numberOfListings);
-        }
     }
   }
 </script>
