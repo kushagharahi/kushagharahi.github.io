8c21fbe8a7a251483b14064e113606d41ca9fa1f
Kusha Gharahi <3326002+kushagharahi@users.noreply.github.com>
2018-11-23 02:29:31 -0600

    closing img tag when rendering markdown

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -85,7 +85,10 @@ module.exports = {
             }
           },
           {
-            loader: 'markdown-loader'
+            loader: 'markdown-loader',
+            options: {
+              xhtml: true
+            }
           }
         ]
       }
