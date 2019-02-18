fb2ea0fa77dd692a62b7224fe92db52a0311232b
Kusha Gharahi <3326002+kushagharahi@users.noreply.github.com>
2019-02-17 23:09:08 -0600

    don't move ico to imgs so it only resides in one place so AggressiveMergingPlugin doesn't link the two

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -72,7 +72,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(png|jpg|gif|svg|ico)$/,
+        test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]',
