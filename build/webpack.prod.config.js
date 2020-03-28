089fa4b0b756674696bc940795f30662916f9d05
Kusha Gharahi <3326002+kushagharahi@users.noreply.github.com>
2020-03-28 01:24:37 -0500

    prerender new blog post

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -13,7 +13,8 @@ const paths = [ '/',
                 '/blog/access_localhost_from_vm_in_host_computer', 
                 '/blog/git_command_line_cheat_sheet', 
                 '/blog/asrock_z77_extreme4_gtx_970_high_sierra_hackintosh',
-                '/blog/managing_sfdx_static_resources_via_npm_or_yarn']
+                '/blog/managing_sfdx_static_resources_via_npm_or_yarn',
+                '/blog/reset_the_root_password_raspbian_image']
                 
 config.mode = 'production'
 /**A plugin for a more aggressive chunk merging strategy. 
