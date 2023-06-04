c17bd96f85c78131361095de0d7a721399edb8ce
Kusha Gharahi <3326002+kushagharahi@users.noreply.github.com>
2023-06-03 22:58:11 -0500

    continue on prod build

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 const config = require('./webpack.dev.config')
-const PrerenderSpaPlugin = require('prerender-spa-plugin')
 const TerserPlugin = require('terser-webpack-plugin');
+const PrerenderSpaPlugin = require('prerender-spa-plugin');
 const SitemapPlugin = require('sitemap-webpack-plugin').default
 const path = require('path')
 const paths = [ '/', 
@@ -15,34 +15,37 @@ const paths = [ '/',
                 '/blog/asrock_z77_extreme4_gtx_970_high_sierra_hackintosh',
                 '/blog/managing_sfdx_static_resources_via_npm_or_yarn',
                 '/blog/reset_the_root_password_raspbian_image']
-                
-config.mode = 'production'
-/**A plugin for a more aggressive chunk merging strategy. 
- * Even similar chunks are merged if the total size is reduced enough. 
- * As option modules that are not common in these chunks can be moved up the chunk tree to the parents. */
-config.plugins.push(new webpack.optimize.AggressiveMergingPlugin())
-// plugin uses terser to minify/minimize your JavaScript. Built into webpack5
-config.optimization.minimizer.push(new TerserPlugin({
-  extractComments: false
-}))
-config.plugins.push(new PrerenderSpaPlugin({
-  // Absolute path to compiled SPA
-  staticDir: path.resolve(__dirname, '../dist'),
-  // List of routes to prerender
-  routes: paths
-}))
-config.plugins.push(new SitemapPlugin({
-  base: 'https://kusha.me', 
-  paths: paths, 
-  options: {
-    skipGzip: true
-  }
-}))
-// Exclude Vue options API during production compile time.
-plugins: [
-  new webpack.DefinePlugin({
-    __VUE_OPTIONS_API__: false
-  })
-]
+module.exports = {
+  mode: 'production',
+  optimization: { 
+    minimizer: [
+      // plugin uses terser to minify/minimize your JavaScript. Built into webpack5
+      new TerserPlugin({
+        extractComments: false
+      })
+    ]
+  },
+  plugins: [
+      /* A plugin for a more aggressive chunk merging strategy. 
+         Even similar chunks are merged if the total size is reduced enough. 
+         As option modules that are not common in these chunks can be moved up the chunk tree to the parents. */
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: false
+    }),
+    new SitemapPlugin({
+      base: 'https://kusha.me', 
+      paths: paths, 
+      options: {
+        skipGzip: true
+      }
+    }),
+    new PrerenderSpaPlugin({
+      // Absolute path to compiled SPA
+      staticDir: path.resolve(__dirname, '../dist'),
+      // List of routes to prerender
+      routes: paths
+    })
+  ]
 
-module.exports = config
+}
