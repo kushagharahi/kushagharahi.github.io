40ba292c45ef5ed414c6ced04f64017e3ea16093
Kusha Gharahi <3326002+kushagharahi@users.noreply.github.com>
2023-06-19 16:55:57 -0500

    url with paths

--- .github/workflows/.lighthouserc.js
+++ .github/workflows/.lighthouserc.js
@@ -1,9 +1,11 @@
 const paths = require('../../build/paths');
 
+const baseUrl = 'localhost:8080'
+
 module.exports = {
   ci: {
     collect: {
-      url: paths,
+      url: paths.map((path) => baseUrl + path),
     },
   },
 };
