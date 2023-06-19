ee51c165d9be49e716831f30c0259b940e9d6c90
Kusha Gharahi <3326002+kushagharahi@users.noreply.github.com>
2023-06-19 17:17:27 -0500

    fix ci

--- .github/workflows/.lighthouserc.js
+++ .github/workflows/.lighthouserc.js
@@ -1,11 +1,12 @@
 const paths = require('../../build/paths');
 
-const baseUrl = 'localhost:8080'
+const baseUrl = 'http://127.0.0.1:8080'
 
 module.exports = {
   ci: {
     collect: {
-      url: paths.map((path) => baseUrl + path),
-    },
+        url:  paths.map((path) => baseUrl + path),
+        startServerCommand: 'npx http-server dist -p 8080',
+      },
   },
 };
